6881808672b20f1fe8b73c75b1ad6e2b896036d9
김예진 <48613073+yejinnnn@users.noreply.github.com>
2022-09-22 15:15:53 +0900

    Add files via upload

--- Femnist/server.py
+++ Femnist/server.py
@@ -1,8 +1,6 @@
 from typing import List, Tuple
 
 import flwr.server.strategy
-from flwr.server.client_manager import SimpleClientManager
-# from fedavg_local import FedAvg
 import flwr as fl
 from flwr.common import Metrics
 
@@ -11,7 +9,6 @@ from flwr.common import Metrics
 if __name__ == "__main__":
 
     def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
-        # Multiply accuracy of each client by number of examples used
         accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
         examples = [num_examples for num_examples, _ in metrics]
 
@@ -20,7 +17,6 @@ if __name__ == "__main__":
         return {"accuracy": sum(accuracies) / sum(examples)}
 
     # Define strategy
-    # client_manager = SimpleClientManager()
     strategy = flwr.server.strategy.FedAvg(min_fit_clients=3, min_available_clients=3, evaluate_metrics_aggregation_fn=weighted_average)
     # server = Server(client_manager=client_manager, strategy=strategy)
 
